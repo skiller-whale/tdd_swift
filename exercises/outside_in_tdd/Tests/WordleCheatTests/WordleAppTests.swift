fa3cfef2adb183664fc389ffc21c327f72a65e5b
Kay Lack <kay@skillerwhale.com>
2026-05-08 12:17:23 +0100

    Address coach feedback

--- exercises/outside_in_tdd/Tests/WordleCheatTests/WordleAppTests.swift
+++ exercises/outside_in_tdd/Tests/WordleCheatTests/WordleAppTests.swift
@@ -2,9 +2,5 @@ import Testing
 @testable import WordleCheat
 
 @Suite struct WordleAppTests {
-    @Test func runReturnsEmptyListForNow() {
-        let app = WordleApp(wordFilePath: "words.txt")
-        let result = app.run(["_____", "FTH", "ISYCA"])
-        #expect(result.isEmpty)
-    }
+    // Add tests for the Wordle app here
 }
